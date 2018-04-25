fab42b4f9e66688a7780453982f7f32ed6a9ad8f
Hidenori Sugiyama <madogiwa@gmail.com>
2018-04-26 00:28:25 +0900

    install fish

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
+#!/bin/sh
 
-## Homebrew
+## install Homebrew
 if [[ "$OSTYPE" =~ darwin ]]; then
     if [ ! -e /usr/local/bin/brew ]; then
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -8,9 +9,13 @@ if [[ "$OSTYPE" =~ darwin ]]; then
     brew bundle --file=Brewfile
 fi
 
-## tpm(tmux package manager)
+## execute rcm
+rcup
+
+## install tpm(tmux package manager)
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-## rcm
-rcup
+## install fisherman
+curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
+fisher
 
