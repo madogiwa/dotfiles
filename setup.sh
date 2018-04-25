9fdc87ead9f76e388a57674d3c3346f32340a885
Hidenori Sugiyama <madogiwa@gmail.com>
2018-04-26 00:05:01 +0900

    add tpm install

--- setup.sh
+++ setup.sh
@@ -8,6 +8,9 @@ if [[ "$OSTYPE" =~ darwin ]]; then
     brew bundle --file=Brewfile
 fi
 
+## tpm(tmux package manager)
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 ## rcm
 rcup
 
