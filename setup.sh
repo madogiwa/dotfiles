33b690ec86cfb5e231e90275ba6e1b825251cb99
Hidenori Sugiyama <madogiwa@gmail.com>
2018-04-27 00:53:11 +0900

    install anyenv

--- setup.sh
+++ setup.sh
@@ -27,4 +27,6 @@ fi
 
 ## install anyenv
 git clone https://github.com/riywo/anyenv ~/.anyenv
+mkdir -p ~/.anyenv/plugins
+git clone https://github.com/znz/anyenv-update.git ~/.anyenv/plugins/anyenv-update
 
