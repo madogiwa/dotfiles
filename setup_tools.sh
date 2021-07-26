ba00bbfd8ad4fb0d21e9fe3eb7f6a0a8c94e2a0b
Hidenori Sugiyama <madogiwa@gmail.com>
2021-07-26 14:16:21 +0900

    add default-npm-packages

--- setup_tools.sh
+++ setup_tools.sh
@@ -1,25 +1,6 @@
 #!/bin/sh
 
 ##
-## npm
-##
-
-npm install -g yarn
-npm install -g typescript
-npm install -g ts-node
-npm install -g typesync
-npm install -g @vue/cli
-npm install -g @vue/cli-init
-npm install -g @angular/cli
-npm install -g @angular/cli
-npm install -g firebase-tools
-npm install -g expo-cli
-npm install -g ionic
-npm install -g serverless
-npm install -g awsp
-npm install -g yo generator-web-extension
-
-##
 ## golang
 ##
 
