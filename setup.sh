c4b5d2e475cccca75222b0ddf508cd5b6b9b78e1
Hidenori Sugiyama <madogiwa@gmail.com>
2021-10-26 23:33:31 +0900

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/zsh
 
 ## install xcode tools
 xcode-select --install
@@ -6,31 +6,29 @@ if [ $? -eq 0 ]; then
     read \?"Waiting xcode-select install... press [Enter] to continue."
 fi
 
-## install Homebrew
-if [[ "$OSTYPE" =~ darwin ]]; then
-    if [ ! -e /usr/local/bin/brew ]; then
-        /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    fi
-
-    brew bundle --file=Brewfile.mas
-    brew bundle --file=Brewfile
-fi
-
-## fix compinit insecure directories
-chmod 755 /usr/local/share/zsh
-chmod 755 /usr/local/share/zsh/site-functions
-
 ## use Xcode bundled Command Line Tools
 echo "switch Command Line Tools to /Application/Xcode.app"
 sudo xcodebuild -license accept
 sudo xcode-select --switch /Applications/Xcode.app
+
+## install Homebrew
+if [ ! -e /usr/local/bin/brew ]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+brew bundle --file=Brewfile.mas
+brew bundle --file=Brewfile
+
+## fix compinit insecure directories
+chmod 755 /usr/local/share/zsh
+chmod 755 /usr/local/share/zsh/site-functions
 
 ## install tpm(tmux package manager)
 rm -rf ~/.tmux/plugins/tpm && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 ## install zinit
 if [[ ! -d ~/.zinit ]]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+    zsh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 fi
 
 ## ssh folder
